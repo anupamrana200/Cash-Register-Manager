25c0fd4eab2591543f601d7d2a0edf47deebd314
anupamrana200 <anupamrana200@gmail.com>
2023-09-04 10:03:35 +0530

    initial js

---
+++ index.js
@@ -0,0 +1,15 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+
+
+checkButton.addEventListener("click", function validateBillAndCashAmount(){
+    message.style.display = "none";
+    if(billAmount.value >0){
+
+    }else{
+        message.style.display = "block";
+        message.innerText = "The Bill Amount Should be greater than 0";
+    }
+} );
