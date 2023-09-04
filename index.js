f655cd478afab8403665b89343fa6cfa6ad218e2
anupamrana200 <anupamrana200@gmail.com>
2023-09-04 17:06:31 +0530

    All logic complete

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
+const lblCashGiven = document.querySelector("#lbl-cash-given");
 const checkButton = document.querySelector("#check-button");
+const nextButton = document.querySelector("#next-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const changeTable = document.querySelector("#change-table");
+
 
 hideMessage();
-const avialableNotes = [2000,500,100,20,10,5,1];
+const avialableNotes = [500,200,100,50,20,10,5,2,1];
+
+nextButton.addEventListener("click",function nextButtonHandler(){
+    showCashGivenCheckBtn();
+});
+
 
 checkButton.addEventListener("click", function validateBillAndCashAmount(){
+    showChangeTable();
     const billInput = parseInt(billAmount.value);
     const cashInput = parseInt(cashGiven.value);
 
@@ -36,6 +46,23 @@ function calculateChange(amount){
 
 function hideMessage(){
     message.style.display = "none";
+
+    cashGiven.style.display = "none";
+    lblCashGiven.style.display = "none";
+
+    checkButton.style.display = "none";
+    changeTable.style.display = "none";
+}
+
+function showCashGivenCheckBtn(){
+    cashGiven.style.display = "block";
+    lblCashGiven.style.display = "block";
+
+    checkButton.style.display = "block";
+}
+
+function showChangeTable(){
+    changeTable.style.display = "table";
 }
 
 
